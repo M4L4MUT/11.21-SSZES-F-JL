51346c95140ad0accecaeb7e92bbf35f12d4b642
M4L4MUT <113418446+M4L4MUT@users.noreply.github.com>
2022-11-21 16:08:46 +0100

    Add files via upload

---
+++ elso.py
@@ -0,0 +1,32 @@
+a="Lustaság"
+b="fél"
+c="egészség!"
+#Há' mükszik a pájton! 
+print(a,b,c) 
+
+
+print(6*(1-2))
+
+
+
+Pista = 6
+print(Pista+4)
+
+
+c = 10000
+r = 8
+m = 12
+t = int(input("Évek száma:"))
+FV = c*(1+(r/m))*m*t
+print(FV)
+
+
+x=int(input("Hány óra múlva csörögjön az óra?"))
+w=int(input("Aktuális idő:"))
+z=w+x
+y=z//24
+if z > 24:
+    z=z-(y*24)
+print(z)
+
+
